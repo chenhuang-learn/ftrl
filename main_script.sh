d4debe7c120a4ca9f086d58a9c0c1ea1adbc1f80
chenhuang-learn <chenhuang.learn@gmail.com>
2015-05-23 00:06:02 +0800

    add main script

---
+++ main_script.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+echo `date`
+
+# ./feature_engineer/count.py train.csv > fc.trva.t10.txt
+
+# ./feature_engineer/parallel_td.py -s 15 ./feature_engineer/transform_data.py train.csv train_trans.csv
+
+# wc -l train.csv train_trans.csv
+
+# python feature_engineer/stat_field_info.py train_trans.csv feature_engineer/st_info_file > /dev/null
+
+./feature_engineer/parallel_gs.py -s 15 -m 3 ./feature_engineer/get_samples.py train_trans.csv train_samples_m3
+
+# java -jar ftrl_train.jar train_samples_m1 1.0 1.0 0.1 1000000 > m1_result
+
+# java -jar ftrl_train.jar train_samples_m2 1.0 1.0 0.1 1086921 > m2_result
+
+echo `date`
+
---
+++ plot_progress_validation.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+# sys.argv[1] : input file
+
+line_indexes = []
+average_loss_1 = []
+average_loss_2 = []
+
+line_index = 0
+for line in open("m1_result"):
+    line = line.strip()
+    fields = line.split(',')
+    if len(fields) == 2:
+        line_indexes.append(line_index)
+        average_loss_1.append(float(fields[1]))
+        line_index += 1
+for line in open("m2_result"):
+    line = line.strip()
+    fields = line.split(',')
+    if len(fields) == 2:
+        average_loss_2.append(float(fields[1]))
+
+line_indexes = np.array(line_indexes)
+average_loss_1 = np.array(average_loss_1)
+average_loss_2 = np.array(average_loss_2)
+print len(line_indexes)
+print len(average_loss_1)
+print len(average_loss_2)
+
+plt.plot(line_indexes, average_loss_1, color='green')
+plt.plot(line_indexes, average_loss_2, color='red')
+plt.show()
+
